2bc23bbb903c223805dbc46983dadd88b40c84f9
문성호 <moonsungho@moonsungho-Mac.local>
2021-05-29 17:55:57 +0900

    s

---
+++ main.js
@@ -0,0 +1,20 @@
+function result() {
+  const father = document.getElementById("father").value;
+  const mother = document.getElementById("mother").value;
+  const result = document.getElementById("result");
+  const height = parseInt(mother) + parseInt(father);
+
+  if (height >= 1) {
+    if (document.getElementById("gender").checked) {
+      const heightn = height + 13;
+      swal("와우", `${heightn / 2}cm네요!`)
+      //result.innerText = `${heightn / 2}cm`;
+    } else {
+      const heightn = height - 13;
+      swal("ㄷㄷ", `${heightn / 2}cm네요!`)
+      //result.innerText = `${heightn / 2}cm`;
+    }
+  } else {
+    swal("에러", "부모님 키를 입력하세요!", "error");
+  }
+}
