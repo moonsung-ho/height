f310d9e96c92937ecb8fe2691eb90e9cbbee9453
sungho0205 <moon@7959.ml>
2021-06-04 17:57:34 +0900

    이상한 놈들이 키를 이상하게 입력하는걸 방지하기위해 부모 키가 합쳐서 300이 되지 않으면 경고 발생

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
+var { default: swal } = require("sweetalert");
+
 function result() {
-  const father = document.getElementById("father").value;//아빠키 입력란 value를 father라고 정의
-  const mother = document.getElementById("mother").value;//엄마키 입력란 value를 mother라고 정의
-  const height = parseFloat(mother) + parseFloat(father);//엄마키와 아빠키를 실수로 변환해서 더함
+  const father = document.getElementById("father").value; //아빠키 입력란 value를 father라고 정의
+  const mother = document.getElementById("mother").value; //엄마키 입력란 value를 mother라고 정의
+  const height = parseFloat(mother) + parseFloat(father); //엄마키와 아빠키를 실수로 변환해서 더함
 
   if (height >= 1) {
-    if (document.getElementById("gender").checked) {
-      const heightn = height + 13;
-      swal("우와", `${heightn / 2}cm네요!`);//결과 alert
+    if (height <= 300) {
+      swal("🙏제발🙏", "키를 제대로 입력해주세요", "warning");
     } else {
-      const heightn = height - 13;
-      swal("와우", `${heightn / 2}cm네요!`);//결과 alert
+      if (document.getElementById("gender").checked) {
+        const heightn = height + 13;
+        swal("우와", `${heightn / 2}cm네요!`); //결과 alert
+      } else {
+        const heightn = height - 13;
+        swal("와우", `${heightn / 2}cm네요!`); //결과 alert
+      }
     }
   } else {
-    swal("에러", "부모님 키를 입력하세요!", "error");//에러
+    swal("에러", "부모님 키를 입력하세요!", "error"); //에러
   }
 }
